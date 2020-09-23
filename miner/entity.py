35b0207519231649750affcf2fce03ee555684be
salt <arcaorm00@gmail.com>
2020-09-23 17:49:32 +0900

    17:49

---
+++ miner/entity.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+@dataclass
+class Entity:
+    context: str = '/Users/saltQ/sbaProject/miner/data/'
+    fname: str = ''
+    target: str = ''
--- titanic/service.py
+++ titanic/service.py
@@ -3,6 +3,16 @@ import sys
 sys.path.insert(0, '/Users/saltQ/sbaProject')
 import pandas as pd
 import numpy as np
+# sklearn algorithm: classification, regression, clustring, reduction 
+from sklearn.tree import DecisionTreeClassifier # dtree
+from sklearn.ensemble import RandomForestClassifier # rforest
+from sklearn.naive_bayes import GaussianNB # nb
+from sklearn.neighbors import KNeighborsClassifier # knn
+from sklearn.svm import SVC # svm
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold # k값은 count의 의미로 이해
+from sklearn.model_selection import cross_val_score
+# dtree, rforest, nb, knn, svm
 
 '''
 ** PassengerId  고객ID, ==> 머신러닝 모델에게 주어질 문제
@@ -50,7 +60,22 @@ class Service:
         return this
 
     @staticmethod
-    def name_nominal(this) -> object:
+    def title_nominal(this) -> object: # 이름 앞의 경칭
+        combine = [this.train, this.test]
+        for dataset in combine:
+            dataset['Title'] = dataset.Name.str.extract('([A-Za-z]+)\.', expand=False)
+        for dataset in combine:
+            dataset['Title'] = dataset['Title'].replace(['Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Jonkheer', 'Dona', 'Mme'], 'Rare')
+            dataset['Title'] = dataset['Title'].replace(['Countess', 'Lady', 'Sir'], 'Royal')
+            dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+            dataset['Title'] = dataset['Title'].replace('Mlle', 'Mr')
+        title_mapping = {'Mr': 1, 'Miss': 2, 'Mrs': 3, 'Master': 4, 'Royal': 5, 'Rare': 6}
+        for dataset in combine:
+            dataset['Title'] = dataset['Title'].map(title_mapping)
+            dataset['Title'] = dataset['Title'].fillna(0)
+        
+        this.train = this.train
+        this.test = this.test
         return this
 
     @staticmethod
@@ -73,8 +98,8 @@ class Service:
         # age를 평균으로 넣기도 애매하고 다수로 넣기도 근거가 없다.
         # 특히 age는 생존률 판단에서 가중치(weight)가 상당하므로 디테일한 접근이 필요하다.
         # 따라서 나이를 모르는 승객은 모르는 상태로 처리해야 값의 왜곡을 줄일 수 있기에
-        # -0.5 라는 경계값으로 처리했다.
-        bins = [-1, 0, 5, 12, 18, 24, 35, 60, np.inf]
+        # -0.5 라는 중간값으로 처리했다. (unknown으로 처리되도록)
+        bins = [-1, 0, 5, 12, 18, 24, 35, 60, np.inf] # 범위
         labels = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'YoungAdult', 'Adult', 'Senior']
         train['AgeGroup'] = pd.cut(train['Age'], bins, labels=labels)
         test['AgeGroup'] = pd.cut(test['Age'], bins, labels=labels)
@@ -88,6 +113,28 @@ class Service:
             6: 'Adult',
             7: 'Senior'
         } # labels를 값으로 처리
+
+        for x in range(len(train['AgeGroup'])):
+            if train['AgeGroup'][x] == 'Unknown':
+                train['AgeGroup'][x] = age_title_mapping[train['Title'][x]]
+        for x in range(len(test['AgeGroup'])):
+            if test['AgeGroup'][x] == 'Unknown':
+                test['AgeGroup'][x] = age_title_mapping[test['Title'][x]]
+        
+        age_mapping = {
+            'Unknown': 0,
+            'Baby': 1, 
+            'Child': 2,
+            'Teenager': 3,
+            'Student': 4,
+            'YoungAdult': 5,
+            'Adult': 6,
+            'Senior': 7
+        }
+        train['AgeGroup'] = train['AgeGroup'].map(age_mapping)
+        test['AgeGroup'] = test['AgeGroup'].map(age_mapping)
+        this.train = train
+        this.test = test
         return this
 
     @staticmethod
@@ -101,8 +148,8 @@ class Service:
     @staticmethod
     def fare_ordinal(this) -> object:
         # []은 변수명, {}은 변수값
-        this.train['FareBand'] = pd.qcut(this['Fare'], 4, labels={1, 2, 3, 4}) # 4등분
-        this.test['FareBand'] = pd.qcut(this['Fare'], 4, labels={1, 2, 3, 4}) 
+        this.train['FareBand'] = pd.qcut(this.train['Fare'], 4, labels={1, 2, 3, 4}) # 4등분
+        this.test['FareBand'] = pd.qcut(this.test['Fare'], 4, labels={1, 2, 3, 4}) 
         return this
     @staticmethod
     def fareBand_nominal(this) -> object: # 요금이 다양하니 클러스터링(그룹화)을 하기 위한 준비
@@ -116,6 +163,37 @@ class Service:
         this.test = this.test.fillna({'Embarked': 'S'}) # 교과서 144p
         # 많은 머신러닝 라이브러리는 클래스 레이블이 *정수*로 인코딩되었을 것이라고 기대한다.
         # 교과서 146p를 보면 문자 blue = 0, green = 1, red = 2로 치환한다. ==> mapping한다.
-        this.train['Embarked'] = this.train['Enbarked'].map({'S': 1, 'C': 2, 'Q': 3})
-        this.test['Embarked'] = this.test['Enbarked'].map({'S': 1, 'C': 2, 'Q': 3})
+        this.train['Embarked'] = this.train['Embarked'].map({'S': 1, 'C': 2, 'Q': 3})
+        this.test['Embarked'] = this.test['Embarked'].map({'S': 1, 'C': 2, 'Q': 3})
         return this
+
+    # Learning Algorithm: dtree, rforest, nb, knn, svm 다섯가지 모듈을 대표로 사용한다.
+
+    @staticmethod
+    def create_k_fold():
+        return KFold(n_splits=10, shuffle=True, random_state=0)
+    
+    def accuracy_by_dtree(self, this):
+        dtree = DecisionTreeClassifier()
+        score = cross_val_score(dtree, this.train, this.label, cv=Service.create_k_fold(), n_jobs=1, scoring='accuracy')
+        return round(np.mean(score) * 100, 2)
+
+    def accuracy_by_rforest(self, this):
+        rforest = RandomForestClassifier()
+        score = cross_val_score(rforest, this.train, this.label, cv=Service.create_k_fold(), n_jobs=1, scoring='accuracy')
+        return round(np.mean(score) * 100, 2)
+
+    def accuracy_by_nb(self, this):
+        nb = GaussianNB()
+        score = cross_val_score(nb, this.train, this.label, cv=Service.create_k_fold(), n_jobs=1, scoring='accuracy')
+        return round(np.mean(score) * 100, 2)
+
+    def accuracy_by_knn(self, this):
+        knn = KNeighborsClassifier()
+        score = cross_val_score(knn, this.train, this.label, cv=Service.create_k_fold(), n_jobs=1, scoring='accuracy')
+        return round(np.mean(score) * 100, 2)
+
+    def accuracy_bt_svm(self,this):
+        svm = SVC()
+        score = cross_val_score(svm, this.train, this.label, cv=Service.create_k_fold(), n_jobs=1, scoring='accuracy')
+        return round(np.mean(score) * 100, 2)
