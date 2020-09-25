97e0c01daf571c26205f89c23c78a5a8405392b9
salt <arcaorm00@gmail.com>
2020-09-25 16:27:01 +0900

    16:26

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask import render_template, request
 from price_prediction.cabbage import Cabbage
+from member.students import Student, StudentService
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('join.html')
 
 # method 종류에는 총 4개가 있다.
 # GET, POST, PUT, DELETE 그래서 이것들은 다같이 array를 이룬다.
@@ -36,6 +37,24 @@ def cabbage():
     render_params = {}
     render_params['result'] = result
     return render_template('index.html', **render_params)
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    print('########## SIGNUP ##########')
+    id = request.form['id']
+    pwd = request.form['pwd']
+    name = request.form['name']
+    birth = request.form['birth']
+
+    student = Student()
+    student.id = id
+    student.pwd = pwd
+    student.name = name
+    student.birth = birth
+    service = StudentService()
+    service.add_students(student)
+
+    
 
 if __name__ == "__main__":
     app.run()
